670ddc1e15b2dbc2e2c6bd1f55a810e4493eefb2
DevExpressExampleBot <fakeEmail>
2021-03-17 03:01:46 +0300

    Source auto update [skip ci]

---
+++ VB/Default.aspx.vb
@@ -0,0 +1,23 @@
+﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardWeb
+Imports DevExpress.DataAccess.Sql
+Imports System
+
+Partial Public Class [Default]
+	Inherits System.Web.UI.Page
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		ASPxDashboard1.SetDataSourceStorage(CreateDataSourceStorage())
+	End Sub
+
+	Public Function CreateDataSourceStorage() As DataSourceInMemoryStorage
+		Dim dataSourceStorage As New DataSourceInMemoryStorage()
+
+		Dim sqlDataSource As New DashboardSqlDataSource("SQL Data Source", "NWindConnectionString")
+		Dim query As SelectQuery = SelectQueryFluentBuilder.AddTable("Products").SelectAllColumnsFromTable().Build("Products")
+		sqlDataSource.Queries.Add(query)
+		dataSourceStorage.RegisterDataSource("sqlDataSource", sqlDataSource.SaveToXml())
+
+		Return dataSourceStorage
+	End Function
+End Class
